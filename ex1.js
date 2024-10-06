5fb8c0959338742c40b849ed5213bcaebce6b409
MochizukiKotaro0621 <24nm763a@gmail.com>
2024-10-06 21:29:39 +0900

    Add files via upload

--- ex1.js
+++ ex1.js
@@ -23,6 +23,44 @@ ctx.transform(dpr, 0, 0, -dpr, 0, height * dpr);
 //canvasアニメーションの作成
 let x = 80, y = 240; // 円の位置
 
+// 軸とメモリを描画する関数
+const drawAxes =()=>{ // drawAxes宣言，()が引数{}が処理
+    //　軸の色を指定
+    ctx.strokeStyle = "black";
+    ctx.lineWidth = 1;
+
+    // x軸の描画
+    ctx.beginPath();
+    ctx.moveTo(0, canvas.height/2);
+    ctx.lineTo(canvas.width, canvas.height/2);
+    ctx.stroke();
+
+    // y軸の描画
+    ctx.biginPath();
+    ctx.moveTo(0,0);
+    ctx.lineTo(0,canvas.height);
+    ctx.stroke();
+
+    //メモリの描画
+    const tickSpacing=50;//メモリの間隔
+    const tickLength=10;//メモリの長さ
+    ctx.linewidth = 1;
+
+    // x軸のメモリ
+    for (let x = 0; x<=canvas.width;x+=tickSpacing){
+        ctx.beginPath();
+        ctx.moveTo(x,canvas.height/2-tickLength/2);
+        ctx.lineTo(x,canvas.height/2+tickLength/2);
+        ctx.stroke();
+    }
+    for (let y = 0; y<=canvas.height;y+=tickSpacing){
+        ctx.beginPath();
+        ctx.moveTo(canvas.width/2-tickLength/2,y);
+        ctx.lineTo(canvas.width/2+tickLength/2,y);
+        ctx.stroke();
+    }
+};
+
 const tick = () => {
   // 画面の消去
   ctx.clearRect(0, 0, canvas.width, canvas.height);
